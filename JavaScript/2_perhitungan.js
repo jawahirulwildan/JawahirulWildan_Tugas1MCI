11a93657bc96cb5b8ca0dc0bd17d96c14860ea88
jawahirulwildan <114124562+jawahirulwildan@users.noreply.github.com>
2023-03-24 23:26:07 +0700

    Update 2_perhitungan.js

--- JavaScript/2_perhitungan.js
+++ JavaScript/2_perhitungan.js
@@ -1,5 +1,4 @@
 import {penjumlahan, pengurangan, perkalian, pembagian} from './fungsi_aritmatika';
-var a = pembagian (4000,2);
-var b = penjumlahan(a, 25) ;
-var c = pengurangan (b, 2);
-console.log(c);
+//4000 / 2 + 25 - 2
+var x = pengurangan ((penjumlahan((pembagian (4000,2)), 25)), 2);
+console.log(x);
